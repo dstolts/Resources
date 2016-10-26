fea8a9fccc602068e376075a4a28b59e7b493b22
Dan Stolts <dstolts@microsoft.com>
2016-10-26 12:55:28 -0400

    Create AzureRMLogin.ps1
    Login to Azure Resource Manager

---
+++ AzureRMLogin.ps1
@@ -0,0 +1,5 @@
+ # Sign-in with Azure account credentials
+Add-AzureAccount   # This adds (caches) your Azure account in PowerShell
+Login-AzureRmAccount # This logs into Azure Resource Manager Account
+# now you can run commands
+Get-AzureRmSubscription
