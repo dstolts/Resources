c250f6b85630fbd69d72f3bc721a0ba8297840dc
Dan Stolts <dstolts@microsoft.com>
2016-10-26 13:49:32 -0400

    Select-Azure-Subscription
    Select Azure Subscription - if multiple, pops a box to allow user to
    select. Assumes ResourceManager

---
+++ Select-Azure-Subscription.ps1
@@ -0,0 +1,25 @@
+﻿# Select Azure Subscription - if multiple, pops a box to allow user to select 
+# Sign-in with Azure account credentials
+Add-AzureAccount
+Login-AzureRmAccount
+
+Param (
+    [Parameter(Mandatory=$false)][string]$SubscriptionID        # Subscription to create storage in
+    ) 
+If ($SubscriptionID -eq "") {
+  # See how many subscriptions are available... 
+  $mySubscriptions = Get-AzureRmSubscription
+  $SubscriptionID = $mySubscriptions[0]  # just grab the first one for now...
+  If ($mySubscriptions.Count -gt 1) { 
+    Write-Host "Select SubscriptionID from the popup list." -ForegroundColor Yellow
+    $subscriptionId = 
+        (Get-AzureRmSubscription |
+         Out-GridView `
+            -Title "Select an Azure Subscription …" `
+            -PassThru).SubscriptionId
+  }
+}
+$mySubscription=Select-AzureRmSubscription -SubscriptionId $subscriptionId
+$SubscriptionName = $mySubscription.Subscription.SubscriptionName
+Write-Host "Subscription: $SubscriptionName $subscriptionId " -ForegroundColor Green
+$mySubscription
