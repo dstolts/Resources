eef92fc2f90f22b7fd22335749524973ff0a3eb9
Dan Stolts <dstolts@microsoft.com>
2018-01-24 14:48:23 -0500

    Add Set-AllowPing Script

---
+++ Set-AllowPing.ps1
@@ -0,0 +1,6 @@
+﻿
+# 
+Get-NetFirewallRule –DisplayName “Allow Ping”
+New-NetFirewallRule –DisplayName “Allow Ping” –Direction inbound –Action Allow –Protocol icmpv4 –Enabled True
+Set-NetFirewallRule –DisplayName “Allow Ping” –Direction inbound –Action Allow –Protocol icmpv4 –Enabled True
+Get-NetFirewallRule –DisplayName “Allow Ping”
